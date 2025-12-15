d80d5c817f88339b1ad12ec53111f45a0a22fa43
mohamed-aly <mali902016@gmail.com>
2025-12-15 10:13:41 +0200

    nested elements

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function Greeting() {
-  return <h1>Hello ReactJS</h1>
+  return (
+    <>
+      <h3>
+        <ul>
+          <li>
+            <a href="https://google.com">HEllo World</a>
+          </li>
+        </ul>
+      </h3>
+    </>
+  )
 }
+
+// function Greeting() {
+//   return React.createElement('h2', {}, 'HEy World!')
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Greeting />)
