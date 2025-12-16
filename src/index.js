c662ac916bd5973232613844237d1aa8f59e6c9e
mohamed-aly <mali902016@gmail.com>
2025-12-16 20:29:26 +0200

    props destructuring

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ const BookList = () => {
   )
 }
 
-const Book = (props) => {
+const Book = ({ job, age }) => {
   return (
     <article className="book">
       <img src="./images/book-1.jpg" alt="The Spear Cuts Through Water" />
       <h2>{title}</h2>
       <h3>{author}</h3>
-      <p>{props.job}</p>
-      <p>{props.age}</p>
+      <p>{job}</p>
+      <p>{age}</p>
     </article>
   )
 }
