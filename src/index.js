c1ffba7ca54f4bb51e68479ab61e8a7b690edbe5
mohamed-aly <mali902016@gmail.com>
2025-12-16 20:21:47 +0200

    props

--- src/index.js
+++ src/index.js
@@ -2,24 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const title = 'The Spear Cuts Through Water'
+const author = 'Simon Jimenez'
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
+      <Book job="developer" />
+      <Book age={22} />
       <Book />
     </section>
   )
 }
 
-const Book = () => {
-  const title = 'The Spear Cuts Through Water'
-  const author = 'Simon Jimenez'
+const Book = (props) => {
   return (
     <article className="book">
       <img src="./images/book-1.jpg" alt="The Spear Cuts Through Water" />
       <h2>{title}</h2>
       <h3>{author}</h3>
+      <p>{props.job}</p>
+      <p>{props.age}</p>
     </article>
   )
 }
